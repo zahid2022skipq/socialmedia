389e48d91433bd3489214eefe9a7c4cabfdc8a39
zahid2022skipq <zahid.ali.skipq@gmail.com>
2023-09-24 14:33:24 +0500

    mobile and desktop navigation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,13 +6,25 @@ import { useState, useEffect } from "react";
 import {signIn, signOut, useSessions, getProviders} from "next-auth/react"
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const isUserLoggedIn = true;
+  // const [providers, setProviders] = useState(null);
+
+
+  // useEffect(() => {
+  //   const setProviders = async () => {
+  //     const response = await getProviders();
+  //     setProviders(response);
+  //   };
+
+  //   setProviders();
+  // }, [])
   return (
     <nav className="flex-between pt-3 mb-16 w-full">
       <Link href="/" className="flex gap-2 flex=center">
         <Image src="/assets/images/logo.svg" alt="logo" width={30} height={30} className="object-contain"/>
         <p className="logo_text">PromptsForYou</p>
       </Link>
+
       {/* Desktop navigation */}
       
         <div className="sm:flex hidden">
@@ -32,9 +44,39 @@ const Nav = () => {
             className="rounded-full"
           />
         </Link>
-      </div>) : (<></>)
+      </div>) : (
+        
+        <>
+          {providers && Object.values(providers).map((provider) => 
+          
+          <button type="button" key={provider.name} onClick={() => signIn(provider.id)} className="black-btn">Sign In</button>
+          )
+          }
+        </>
+        
+        )
       }
       </div>
+
+      
+      {/* Mobile navigation */}
+
+      <div className="sm:hidden flex" >
+          {isUserLoggedIn ? <div className="flex">
+            <Image src="/assets/images/logo.svg" height={37} width={37} alt="profile" />
+          </div>
+          :
+          (  <>
+          {providers && Object.values(providers).map((provider) => 
+          
+          <button type="button" key={provider.name} onClick={() => signIn(provider.id)} className="black-btn">Sign In</button>
+          )
+          }
+        </>
+          )
+          }
+      </div>
+      
     </nav>
   )
 }
